baa04d4f394df96f2b522ecd4c208521762abea1
Peter Macdonald <peter@notmatter.com>
2016-06-09 17:08:53 -0700

    Added ability to load from images, cleaned up code a bit

--- image.py
+++ image.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python
 
 import numpy as np
-import scipy.misc as smp
+import scipy.misc as spm
 import math
 from random import *
 import time
 
-t = time.clock()
-dt = time.clock() - t
-print "Null time: %f" % dt
-t = time.clock()
-
-HEIGHT = 500
-WIDTH = 1000
+D = 100
 
 def dist(x1,y1,x2,y2):
 	return math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
-
-
-#data[512,512] = [254,0,0]       # Makes the middle pixel red
-#data[512,513] = [0,0,255]       # Makes the next pixel blue
 
 # Generate interesting repeating pattern image
 def genRandPattern(height,width):
@@ -29,14 +19,8 @@ def genRandPattern(height,width):
 			pat[y,x] = [randint(0,255),randint(0,255),randint(0,255)]
 	return pat
 
-pattern = genRandPattern(100,100)
-
-dt = time.clock() - t
-print "Init: %f" % dt
-t = time.clock()
-
 # Generate image to be 3d
-def genTestImage(width,height):
+def genTestDepthMap(width,height):
 	img = np.zeros( (height,width,1), dtype=np.uint8 )
 	for x in range(width):
 		for y in range(width):
@@ -45,15 +29,11 @@ def genTestImage(width,height):
 				img[y,x] = d/10
 	return img
 
-image = genTestImage(WIDTH,HEIGHT)
+def loadDepthMap(img):
+	return spm.imread(img, flatten=False, mode='L')
 
-dt = time.clock() - t
-print "Image generation: %f" % dt
-t = time.clock()
-
-D = 100
 def genShiftMap(img,width,height):
-	''' 
+	'''
 	Shift of z where z is the height causes a rise at x, and a fall at x+D
 	'''
 	# Apply shift algorithm to make the image 3D
@@ -61,16 +41,10 @@ def genShiftMap(img,width,height):
 	for y in range(height):
 		for x in range(width):
 			if ( img[y,x] > 0 ):
-				for x2 in range(x,width,D):	
-					shift_map[y,x2] += img[y,x]
+				for x2 in range(x,width,D):
+					shift_map[y,x2] += img[y,x]/20
 	return shift_map
 
-shiftmap = genShiftMap(image,WIDTH,HEIGHT)
-
-dt = time.clock() - t
-print "Shift map: %f" % dt
-t = time.clock()
-	
 def genMagicImg(shift_map,pattern,width,height):
 	magic_img = np.zeros( (height,width,3), dtype=np.uint8 )
 	for y in range(height):
@@ -81,16 +55,52 @@ def genMagicImg(shift_map,pattern,width,height):
 			magic_img[y,x] = pattern[py,px]
 	return magic_img
 
-data = genMagicImg(shiftmap,pattern,WIDTH,HEIGHT)
 
-dt = time.clock() - t
-print "Magic image generation: %f" % dt
-t = time.clock()
-	
-img = smp.toimage( data )       # Create a PIL image
-img.show() 
+if __name__ == "__main__":
+	t = time.clock()
+	dt = time.clock() - t
+	print "Null time: %f" % dt
+#####################
+	t = time.clock()
 
-dt = time.clock() - t
-print "Output: %f" % dt
-t = time.clock()
-	
+	pattern = genRandPattern(100,100)
+
+	dt = time.clock() - t
+	print "Init: %f" % dt
+#####################
+	t = time.clock()
+
+	#HEIGHT = 500
+	#WIDTH = 1000
+	#image = genTestDepthMap(WIDTH,HEIGHT)
+
+	image = loadDepthMap("./depth_maps/depth4.png")
+	HEIGHT = image.shape[0]
+	WIDTH = image.shape[1]
+	print WIDTH,HEIGHT
+
+
+	dt = time.clock() - t
+	print "Image generation: %f" % dt
+	t = time.clock()
+
+	shiftmap = genShiftMap(image,WIDTH,HEIGHT)
+
+	dt = time.clock() - t
+	print "Shift map: %f" % dt
+#####################
+	t = time.clock()
+
+	data = genMagicImg(shiftmap,pattern,WIDTH,HEIGHT)
+
+	dt = time.clock() - t
+	print "Magic image generation: %f" % dt
+#####################
+	t = time.clock()
+
+	#img = spm.toimage( data )       # Create a PIL image
+	#img.show()
+	spm.imsave("out.png",data)
+
+	dt = time.clock() - t
+	print "Output: %f" % dt
